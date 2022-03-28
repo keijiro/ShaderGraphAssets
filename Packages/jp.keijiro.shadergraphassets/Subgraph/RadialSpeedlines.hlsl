88d295f4619385a0392227c7e70dbc0dc5a40836
Keijiro Takahashi <keijiro@gmail.com>
2022-03-29 00:56:42 +0900

    Speedlines shader refinement

--- Packages/jp.keijiro.shadergraphassets/Subgraph/RadialSpeedlines.hlsl
+++ Packages/jp.keijiro.shadergraphassets/Subgraph/RadialSpeedlines.hlsl
@@ -1,24 +1,37 @@
 float RadialSpeedlines
   (float2 UV, float Freq, float2 Thick, float2 Radius, float Seed)
 {
-    float2 npos = (UV - 0.5) / sqrt(0.5);
+    float2 npos = (UV - 0.5) * 2;
 
+    // Polar coordinates
     float2 polar;
     polar.x = (atan2(npos.x, npos.y) / PI + 1) / 2;
     polar.y = length(npos);
 
-    uint seed = (uint)((polar.x + Seed) * Freq) * 4;
-
+    // Spoke number / parameter in spoke
+    uint spoke = (uint)((polar.x + Seed) * Freq) * 4;
     float param = frac(polar.x * Freq) * 2 - 1;
 
-    float offs = (Hash(seed) - 0.5) * Thick.y;
-    float width = Thick.x * (1 - Hash(seed + 1) * Thick.y) * (1 - abs(offs));
-    float radius = saturate(Radius.x + (Hash(seed + 2) - 0.5) * Radius.y);
+    // Random number generator
+    float r1 = Hash(spoke) * 2 - 1;
+    float r2 = Hash(spoke + 1);
+    float r3 = Hash(spoke + 2);
 
-    width *= (polar.y - radius) / (1.001 - radius);
-    width *= width <= 0 ? 100 : 1;
+    // Spoke offset / width
+    float offs = Thick.y * r1 * 0.95;
+    float width = Thick.x * (1 - abs(offs)) * (1 - Thick.y * r2);
 
-    return 1 - saturate((abs(param + offs) - width) * 1000 / Freq / polar.y);
+    // Extent toward outer ring
+    float inner_ring = Radius.x + Radius.y * r3;
+    width *= smoothstep(inner_ring, inner_ring + 1, polar.y);
+
+    // Thresholding with pseudo antialiasing
+    // Equivalent to [ abs(param + offs) <= width ] but with AA.
+    float coeff = 1000;
+    float pot = width * (1 + Freq / coeff) - abs(param + offs);
+    float level = saturate(pot * coeff / Freq);
+
+    return level;
 }
 
 void RadialSpeedlines_float
